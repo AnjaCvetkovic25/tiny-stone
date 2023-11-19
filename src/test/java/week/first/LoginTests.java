b3c33fa61b2100bab5d1f65fb878eef24fccb3dc
Anja <anja.majacvetkovic@gmail.com>
2023-11-19 21:50:45 +0100

    zadatak 1: napisan test za uspesno logovanje

---
+++ src/test/java/week/first/LoginTests.java
@@ -0,0 +1,62 @@
+package week.first;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.openqa.selenium.WebDriver.*;
+
+public class LoginTests {
+    @Test
+    public void positiveLoginTest(){
+        //List<String> usernames = Arrays.asList("standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user", "visual_user");
+        String username = "standard_user";
+        String password = "secret_sauce";
+
+
+        FirefoxOptions options = new FirefoxOptions().setBinary("/usr/sbin/firefox");
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver(options);
+
+        driver.get("https://www.saucedemo.com/");
+
+
+
+        WebElement usernameFieldCSS = driver.findElement(By.cssSelector("input[id='user-name']"));
+
+        WebElement passwordFieldCSS = driver.findElement(By.cssSelector("input[type='password']"));
+
+        //usernameFieldCSS.sendKeys("secret_sauce");
+
+        WebElement logInButtonCSS = driver.findElement(By.cssSelector("input[value='Login']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+        usernameFieldCSS.sendKeys(username);
+        passwordFieldCSS.sendKeys(password);
+
+        logInButtonCSS.click();
+
+        wait.until(ExpectedConditions.titleIs("Swag Labs"));
+
+        WebElement menu = driver.findElement((By.id("react-burger-menu-btn")));
+        menu.click();
+
+        WebElement logOutButton = driver.findElement(By.id("logout_sidebar_link"));
+        logOutButton.click();
+
+        driver.close();
+    }
+}
